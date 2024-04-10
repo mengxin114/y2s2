30d56076935b5a01e80c5e23a8aaf72f2d1d28a3
mengxin114 <1287961101@qq.com>
2024-04-10 16:29:05 +0800

    bug fix

--- skeleton code.c
+++ skeleton code.c
@@ -1,14 +1,14 @@
 #include "maze.h"
 
 int win = 0;
-char in = ''; // current input
+char in = '\0'; // current input
 
 int x;
 int y;   // the location of player
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
+    if (argc != 4)
     {
         printf("argument error");
         return 0;
@@ -16,30 +16,31 @@ int main(int argc, char *argv[])
     else
         printf("correct arguments");
 
-    createMaze(argc[2], argc[3]);
+    createMaze(atoi(argv[2]), atoi(argv[3]));
 
-    loadMap(argc[1]); // load map and get the location of 'S'
+    loadMap(argv[1]); // load map and get the location of 'S'
 
     while (!win)
     {
         in = getInput(); // get legal input
         if (in == 'M')
-            showMap();
-        elif (in == 'X')
         {
+            showMap();
+        }
+        else if (in == 'X'){
             closeMap();
             show();
         }
-        elif (in == '')
+        else if (in == '\0'){
             show();
-        else
-        {
+        }
+        else{
             move(in); // if can pass,then move
             show();
             checkWin(); // check whether win the game
         }
-        in = ''; // reset the input
+        in = '\0'; // reset the input
     }
-    freeMaze(argc[2], argc[3]);
+    freeMaze(atoi(argv[2]), atoi(argv[3]));
     return 0;
 }
